7d518c3c38e0d4e56bf93024fa2faf4690e75623
grisselfaura <grissel.faura@gmail.com>
2020-01-06 21:53:53 +0000

    Implement emailjs integration on the contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "grissel_resumen_test", {
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+}
